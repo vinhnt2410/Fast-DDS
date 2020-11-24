58720ac90cb6f6029c4283d58ee616c70cf108ce
Iker Luengo <ikerluengo@eprosima.com>
2020-11-24 11:14:58 +0100

    Refs 9671. Initialize data sharing domain ID on QoS
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- include/fastdds/dds/core/policy/QosPolicies.hpp
+++ include/fastdds/dds/core/policy/QosPolicies.hpp
@@ -2896,7 +2896,7 @@ private:
     std::string shm_directory_;
 
     //! Only endpoints with matching domain IDs are DataSharing compatible
-    uint64_t domain_id_;
+    uint64_t domain_id_ = 0;
 };
 
 /**
