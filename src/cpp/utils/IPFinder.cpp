f606866edb8820a8b336dc63b4972d4732549fe5
Iker Luengo <ikerluengo@eprosima.com>
2020-11-26 17:55:26 +0100

    Refs 9912. Host calculates a new ID based on macs
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- src/cpp/utils/IPFinder.cpp
+++ src/cpp/utils/IPFinder.cpp
@@ -191,6 +191,37 @@ bool IPFinder::getIPs(std::vector<info_IP>* vec_name, bool return_loopback)
     freeifaddrs(ifaddr);
     return true;
 }
+
+#endif
+
+#if defined(_WIN32)
+
+bool IPFinder::getAllMACAddress(std::vector<info_MAC>* macs)
+{
+    return false;
+}
+
+#elif defined(__APPLE__)
+
+bool IPFinder::getAllMACAddress(std::vector<info_MAC>* macs)
+{
+    return false;
+}
+
+#elif defined(__linux__)
+
+bool IPFinder::getAllMACAddress(std::vector<info_MAC>* macs)
+{
+    return false;
+}
+
+#else
+
+bool IPFinder::getAllMACAddress(std::vector<info_MAC>* macs)
+{
+    return false;
+}
+
 #endif
 
 bool IPFinder::getIP4Address(LocatorList_t* locators)
